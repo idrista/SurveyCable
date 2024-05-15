634c988f766aeefea4a1920e516b38228e0cf278
Idris TAKERBOUST <idris@takerboust.fr>
2024-05-15 10:19:44 +0200

    feat: force the registration by making a session cookie

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  before_action :should_be_registered
+
+  def registered_user
+    session[:id]
+  end
+
+  private
+  def should_be_registered
+    unless session[:id]
+      session[:referer] = request.url
+      redirect_to :register
+    end
+  end
 end
